84988455e57fc4c0bbfae1994d6fe150dc825891
EvgeniyKvasov <eakvasov@yandex.ru>
2025-11-26 18:33:01 +0300

    Task2 with added user tests

--- task2.py
+++ task2.py
@@ -19,6 +19,20 @@ def user_input():
     q = float(input("Введите направление движения спасателя по песку (в градусах):"))
     
     return d1, d2, h, v, n, q
+    
+def test_user_input():
+
+    print("Тест пользовательского ввода.")
+    
+    print("d1 > 0:", d1 > 0)
+    
+    print("d2 > 0:", d2 > 0)
+    
+    print("h > 0:", h > 0)
+    
+    print("v > 0:", v > 0)
+    
+    print("n > 0:", n > 0)
  
 def task_calculation(d1, d2, h, v, n, q):
 
@@ -30,7 +44,17 @@ def task_calculation(d1, d2, h, v, n, q):
 
     t = (1/(v*5280/3600))*(l1 + (n*l2))
     
-    return t
+    return t, l1, l2
+    
+def test_task_calculation(t, l1, l2):
+    
+    print("Тест вычислений программы.")
+    
+    print("l1 > 0:", l1 > 0)
+    
+    print("l2 > 0:", l2 > 0)
+    
+    print("t > 0", t > 0)
     
 def calculation_output(q, t):
 
@@ -38,8 +62,20 @@ def calculation_output(q, t):
 
     print(f"Если спасатель начнёт движение под углом равным {int(q)} градусам, он достигнет утопащего через {t:.1f} секунд.")
     
+def test_calculation_output(t):
+    
+    print("Тест вывода результатов программы.")
+    
+    print("t > 0:", t > 0)
+    
 d1, d2, h, v, n, q = user_input()
 
-t = task_calculation(d1, d2, h, v, n, q)
+test_user_input()
+
+t, l1, l2 = task_calculation(d1, d2, h, v, n, q)
+
+test_task_calculation(t, l1, l2)
 
 calculation_output(q, t)
+
+test_calculation_output(t)
